42f952fdea91fa214e5c720c2ab4a2124523e34e
win10 <win10@GZM158>
2017-04-06 13:24:14 +0800

    04061324

---
+++ WebOrderUI/Controllers/OperContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IBLL;
+using DI;
+
+namespace MvcWebOrder.Controllers
+{
+    public class OperContext
+    {
+        private IBLLSession _BLLSession;
+        public IBLLSession BLLSession
+        {
+            get
+            {
+                if (_BLLSession==null)
+                {
+                    IBLLSessionFactory bllSessionFactory = SpringHelper.GetObject<IBLLSessionFactory>("BLLSessionFactory");
+                    _BLLSession = bllSessionFactory.GetBLLSession();
+                }
+                return _BLLSession;
+            }
+        }
+    }
+}
