8a95fe6e34457a9f3ba5ac5dfd133ea8b539b1e9
win10 <win10@GZM158>
2017-04-13 00:15:49 +0800

    0413-0015

--- WebOrderUI/Userjs/showuser.js
+++ WebOrderUI/Userjs/showuser.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
     var datagrid;
-    datagrid=$('#datagrid').datagrid({
+    datagrid = $('#datagrid').datagrid({
+        fitColumns:true,
         url: "/Admin/ShowUsers",
         title: "用户列表",
         iconCls: "icon-save",
@@ -12,28 +13,28 @@
         columns: [[
             { field: 'ID', checkbox: true },
             { field: 'UserName', title: '用户名', width: 80 },
-            { field: 'LoginTime', title: '最后登录时间', width: 140, align:center, formatter:formatDatebox,editor:'datebox'   }
-        ]
-        ],
-     });
+            { field: 'LoginTime', title: '最后登录时间', width: 140, formatter:formatDatebox,editor:'datebox'   }
+        ]],
+    });
+
+    serializeObject = function (form) {
+        var o = [];
+        $.each(form.serializeArray(), function (index) {
+            if (o[this["name"]]) {
+                o[this["name"]] = o[this["name"]] + "," + this["value"];
+            } else {
+                o[this["name"]] = this["value"];
+            }
+        });
+        return o;
+    };
+    function _search() {
+        datagrid.datagrid('load', serializeObject($('#searchForm')));
+    }
 
      function clearSearch() {
          datagrid.datagrid('load', {});
          $('#searchForm input').val('');
      }
-    /*
-var datagrid;
-    $.ajax({
-        url: '/Admin/ShowUsers',
-        type: 'post',
-        datatype: 'json',
-        success: function (data) {
-            datagrid = $('#datagrid');
-            datagrid.datagrid({
-                columns: [data.title],
-            });
-           // datagrid.datagrid('loadData', data.rows); 
-        }
-    })
-    */
+
 })
