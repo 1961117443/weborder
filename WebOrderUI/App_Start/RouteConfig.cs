6fc2a170c292376048fb048d29de53d803e596bd
win10 <win10@GZM158>
2017-04-26 21:17:19 +0800

    0426-2117

--- WebOrderUI/App_Start/RouteConfig.cs
+++ WebOrderUI/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace MvcWebOrder
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "AdminLogin", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "AdminLogin", action = "Login", id = UrlParameter.Optional },
+                namespaces: new string[] { "MvcWebOrder.Controllers" }
             );
         }
     }
